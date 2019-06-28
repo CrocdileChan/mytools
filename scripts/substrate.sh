d4544f1fa6efff537bab29e3b51ab7d324a48f24
CrocdileChan <CrocdileChan@github.com>
2019-06-28 11:18:43 +0800

    - to --

--- scripts/substrate.sh
+++ scripts/substrate.sh
@@ -1,4 +1,4 @@
-git clone -depth=1 https://github.com/paritytech/substrate
+git clone --depth=1 https://github.com/paritytech/substrate
 
 cd substrate/node-template && git checkout -b v1.0 && ./scripts/init.sh && ./scripts/build.sh && cargo build --release
 
