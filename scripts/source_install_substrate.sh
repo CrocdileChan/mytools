2075a1575e6d8a6302100d021cc294b2794adf6c
CrocdileChan <CrocdileChan@github.com>
2019-07-08 00:37:04 +0800

    -b

--- scripts/source_install_substrate.sh
+++ scripts/source_install_substrate.sh
@@ -1,6 +1,6 @@
-git clone --depth=1 https://github.com/paritytech/substrate
+git clone -b v1.0 --depth=1 https://github.com/paritytech/substrate
 
-cd substrate/node-template && git checkout -b v1.0 && sh ./scripts/init.sh && sh ./scripts/build.sh && cargo build --release
+cd substrate/node-template  && sh ./scripts/init.sh && sh ./scripts/build.sh && cargo build --release
 
 
 echo "******************************* build finished ! ! ! ************************************"
