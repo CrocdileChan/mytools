141224436f9e5308913f422d21df636b1239d3b1
CrocdileChan <CrocdileChan@github.com>
2019-09-01 09:21:07 +0800

    ipfs.sh

---
+++ scripts/ipfs.sh
@@ -0,0 +1,5 @@
+wget https://github.com/ipfs/go-ipfs/releases/download/v0.4.22/go-ipfs_v0.4.22_linux-amd64.tar.gz
+tar xzf go-ipfs_v0.4.22_linux-amd64.tar.gz
+cd go-ipfs && ./install.sh
+ipfs init
+ipfs id
