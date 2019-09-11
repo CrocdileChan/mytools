00e71af861b2a724227f687333c8555468294407
CrocdileChan <CrocdileChan@github.com>
2019-09-11 13:43:18 +0800

    modify ipfs.sh

--- scripts/ipfs.sh
+++ scripts/ipfs.sh
@@ -1,5 +1,6 @@
-wget https://github.com/ipfs/go-ipfs/releases/download/v0.4.22/go-ipfs_v0.4.22_linux-amd64.tar.gz
-tar xzf go-ipfs_v0.4.22_linux-amd64.tar.gz
+wget https://github.com/ipfs/go-ipfs/releases/download/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz
+tar xzf go-ipfs_v0.4.17_linux-amd64.tar.gz
 cd go-ipfs && ./install.sh
 ipfs init
-ipfs id
+echo "******************* ipfs run by daemon  ************************"
+ipfs daemon &
